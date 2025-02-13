ab13b12528e801a6b11257f710e190a464e3be0a
alexsant22 <alexandre.santana2201@gmail.com>
2025-02-13 17:56:33 -0300

    Exemplos - Time

--- Aulas/TimesBiblioteca/time.py
+++ Aulas/TimesBiblioteca/time.py
@@ -1,18 +1,19 @@
 import time
+import random
 
 # Função para simular um processo demorado
 def tarefa_demorada():
     print("Iniciando a tarefa...")
-    time.sleep(3) # Pausa a execução por 2seg
-    print("Tarefa concluida!")
+    time.sleep(random.uniform(1, 10))  # Pausa a execução por um tempo aleatório entre 1 e 10 segundos
+    print("Tarefa concluída!")
 
 # Medir o tempo de execução
-inicio = time.time() # Marca o tempo no inicio da tarefa
+inicio = time.time()  # Marca o tempo no inicio da tarefa
 
 # Executando a função
-tarefa_demorada() # Marca o tempo de inicio da tarefa
+tarefa_demorada()  # Marca o tempo de inicio da tarefa
 
-fim = time.time() # Marca o tempo final da tarefa
+fim = time.time()  # Marca o tempo final da tarefa
 
 # Calculando o tempo total de execução
 tempo_execucao = fim - inicio
